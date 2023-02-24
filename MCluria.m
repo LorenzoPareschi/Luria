2ba6e074640a651102abbb824d73e6c2c04a5c71
LorenzoPareschi <126281144+LorenzoPareschi@users.noreply.github.com>
2023-02-24 21:17:40 +0100

    Add files via upload

--- MCluria.m
+++ MCluria.m
@@ -1,7 +1,7 @@
 % From "The kinetic theory of mutation rates"
 % by L.Pareschi and G.Toscani, Axioms 2023
 %
-% Simulation of the Barlett formulation
+% Simulation of the Luria-Delbrick and Lea-Coulson formulations
 
 close all;
 clear all;
@@ -16,20 +16,36 @@ set(0,'DefaultTextFontSize',20)
 set(0,'DefaultAxesFontSize',16)
 set(0,'DefaultLineLineWidth',2);
 
+% Model selection
+
+LD=1;         % 1=Lura-Delbruck, 2=Lea-Coulson
+if LD==1
+    model='Luria-Delbr\"uck case';
+else
+    model='Lea-Coulson case';
+end
+
 % Parameters
 
 epsilon=0.01; % scaling factor
 t=6.7;        % final time
 beta1=3;      % grow rate of normal cells
-beta2=2.8;    % grow rate of mutants cells
 mu=10^(-7);   % per-cell per-unit-time mutation rate
 n=300;        % number of mutant cells
 dt=0.1;       % time step
-N=100000;     % number of particles
+N=100000;      % number of particles
 
-% Reference solution
+% Exact solution
 
-lcexact=lc2(beta1,beta2,mu,t,n);
+n=300;
+
+if LD==1
+    beta2=2.5; % grow rate of mutants cells
+    ldexact=ld(beta1,beta2,mu,t,n);
+else
+    beta2=2.8; % grow rate of mutants cells
+    ldexact=lc2(beta1,beta2,mu,t,n);
+end
 
 % Scaling of the variables
 
@@ -39,23 +55,24 @@ mu=mu*epsilon;
 beta1=beta1*epsilon;
 alpha=beta1+mu;
 
-% Initializations
+% Initialize variables
 
 mc=zeros(1,N);
-nc=ones(1,N);
-md=zeros(1,N);
 
-
-Ne(1)=1;
+Nc(1)=1;
 Me(1)=0;
 Ma(1)=0;
+Vc(1)=0;
 Mc(1)=0;
 
-k=0;
+k=1;
 
-% Barlett evolution
-   
 for t=dt:dt:T
+    k=k+1;
+    
+    % Evolution of normal cells
+    
+    Nc(k)=Nc(1)*exp(beta1*t);
     
     % Nm <= N is the number of interacting particles
     
@@ -64,46 +81,36 @@ for t=dt:dt:T
     mp=randperm(N);
     mi=mp(1:Nm);
     
-    eta=poissrnd(beta1*nc(mi));
+    eta=poissrnd(mu*Nc(k-1),1,Nm);
     
-    zeta=poissrnd(mu*nc(mi));
-    
-    mp=randperm(N);
-    mi=mp(1:Nm);
-    
-    theta=poissrnd(beta2*mc(mi));
-    
-    mc(mi)=mc(mi)+theta+zeta;   
-    nc(mi)=nc(mi)+eta;
-    
+    if LD==1
+        theta=beta2*mc(mi);
+    else
+        theta=poissrnd(beta2*mc(mi));
+    end
+   
+    mc(mi)=mc(mi)+theta+eta;
+
     if round(k/50)*50==k
         disp(sprintf('time=%3.2f',t*epsilon));
     end
     
-    k=k+1;
-    
 end
 
-disp(sprintf('time=%3.2f',t*epsilon));
 [f,mm]=hist(mc(mc<=n),100);
 dm=mm(2)-mm(1);
 f=f/(dm*N);
 plot(mm,f,':o');
 hold on;
-plot(lcexact,'-k');
+plot(ldexact,'-k');
 xlabel('mutations $v$');
 ylabel('$f(v,t)$');
-
-title(sprintf('Barlett case: $\\varepsilon=%g$',epsilon));
+title(sprintf('%s $:\\varepsilon=%g$',model,epsilon));
 legend('Kinetic model','Reference solution');
-axis([0 n 0 0.014]);
 hold off;
+if LD==1
+    axis([0 n 0 0.018]);
+else
+    axis([0 n 0 0.014]);
+end
 drawnow;
-
-figure;
-histogram2(mc(mc<=200),nc(mc<=200),100,'FaceColor','flat','ShowEmptyBins','on');
-view(0,90);
-title(sprintf('Barlett formulation: $\\varepsilon=%g$',epsilon));
-xlabel('mutated cells $v$');
-ylabel('normal cells $w$');
-
--- ld.m
+++ ld.m
@@ -1,4 +1,4 @@
-function p=lc2(beta1,beta2,mu,t,n)
+function p=ld(beta1,beta2,mu,t,n)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Implementation from 
@@ -6,21 +6,12 @@ function p=lc2(beta1,beta2,mu,t,n)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 rho=beta1/beta2;
+
 m=(mu/beta1)*(exp(beta1*t)-1);
 theta=(mu/beta1)*exp(beta1*t);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% This assumes kappa -1 >= n
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-zz=mu*exp(-beta2*t);
-dt=t/10000;
-s=0:dt:t;
-    
-for j=1:n+1
-    y=qLC(s,beta1,beta2,j,t);
-    qj=trapz(s,y);
-    lambda(j)=zz*qj;
+for j=1:n+1;
+    lambda(j)=theta*(j^(-rho)-(j+1)^(-rho));
 end
 
 q(1)=-m;
@@ -30,10 +21,11 @@ q(2:n+1)=lambda(1:n);
 p=zeros(n+1,1);
 p(1)=exp(q(1));
 
-for i=2:n
+for i=2:n;
     pp=0.0;
     for j=0:i-1
        pp=pp+(i-1-j)*q(i-j)*p(j+1);
     end
     p(i)=(1/(i-1))*pp;
 end
+
